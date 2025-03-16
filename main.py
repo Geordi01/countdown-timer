0c5533c0c8fb2d8b8fd0bedad1d0a65857d5fc05
Geordi01 <gjad.bonnie@gmail.com>
2025-03-16 21:24:21 +0100

    Able to pause

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from tkinter import *
 
-
-
 reset_timer = False
+pause_timer = False
 
 def remove():
-    global reset_timer
+    global reset_timer, pause_timer
     if reset_timer == False:
         start_button.grid_remove()
         time_entry_hours.grid_remove()
@@ -17,9 +16,16 @@ def remove():
     elif reset_timer == True:
         pause_button.grid_remove()
         reset_button.grid_remove()
+        resume_button.grid_remove()
+        print(pause_button.display, resume_button.display)
+    
+    if pause_timer == False and reset_timer == False:
+        resume_button.grid_remove()
+    elif pause_timer == True and reset_timer == False:
+        pause_button.grid_remove()
 
 def display():
-    global reset_timer
+    global reset_timer, pause_timer
     if reset_timer == False:
         pause_button.grid(column=0, row=0)
         reset_button.grid(column=1, row=0)
@@ -31,10 +37,16 @@ def display():
         time_label_hours.grid(column=1, row=1)
         time_label_minutes.grid(column=1, row=2)
         time_label_seconds.grid(column=1, row=3)
+    
+    if pause_timer == False and reset_timer == False:
+        pause_button.grid(column=0, row=0)
+    elif pause_timer == True and reset_timer == False:
+        resume_button.grid(column=0, row=0)
 
 def start_time():
-    global reset_timer, time_in_seconds, time_hours, time_minutes, time_seconds
+    global reset_timer, pause_timer, time_in_seconds, time_hours, time_minutes, time_seconds
     reset_timer = False
+    pause_timer = False
     remove()
     display()
     time_hours = int(time_entry_hours.get())
@@ -50,13 +62,11 @@ def start_time():
     time_text.config(text=f"{time_hours}:{time_minutes}:{time_seconds}")
     countdown()
 
-def pause_time():
-    pass
-
 def countdown():
     global time_in_seconds, time_hours, time_minutes, time_seconds
+    print(f"paused: {pause_timer}, reset: {reset_timer}")
 
-    if time_in_seconds > 0 and reset_timer == False:
+    if time_in_seconds > 0 and reset_timer == False and pause_timer == False:
         time_in_seconds -= 1
         time_hours = time_in_seconds // 3600
         time_minutes = (time_in_seconds // 60) - 60
@@ -71,17 +81,34 @@ def countdown():
         time_text.after(1000, countdown)
     elif reset_timer == True:
         return
+    elif pause_timer == True:
+        time_text.config(text=f"{time_hours}:{time_minutes}:{time_seconds}")
     else:
         time_text.config(text="Time's Up!")
 
+def pause_time():
+    global pause_timer
+    pause_timer = True
+    remove()
+    display()
+
+def resume_time():
+    global pause_timer
+    pause_timer = False
+    remove()
+    display()
+    countdown()
+
 def reset():
-    global reset_timer, time_in_seconds, time_hours, time_minutes, time_seconds
+    global reset_timer, pause_timer, time_in_seconds, time_hours, time_minutes, time_seconds
     reset_timer = True
+    pause_timer = False
     time_in_seconds = 0
     time_hours = 0
     time_minutes = 0
     time_seconds = 0
     time_text.config(text="00:00")
+    print(f"paused: {pause_timer}, reset: {reset_timer}")
     remove()
     display()
 
@@ -120,10 +147,12 @@ button_frame.grid(column=0, row=4)
 time_in_seconds = time_hours * 3600 + time_minutes * 60 + time_seconds
     
 start_button = Button(button_frame, text="Start", command=start_time)
-pause_button = Button(button_frame, text="Pause")
+pause_button = Button(button_frame, text="Pause", command=pause_time)
 reset_button = Button(button_frame, text="Reset", command=reset)
+resume_button = Button(button_frame, text="Resume", command=resume_time)
 pause_button.display = False
 reset_button.display = False
+resume_button.display = False
 
 start_button.grid(column=1, row=0)
 
